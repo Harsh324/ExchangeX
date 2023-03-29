b7f779341980f50e45e5f28b7120fc5be9083510
Harsh_324 <harshravi324@gmail.com>
2023-03-29 18:51:10 +0530

    Payment in progress

--- Payment/payment/payment_functions.py
+++ Payment/payment/payment_functions.py
@@ -1,17 +1,41 @@
+import sqlite3 as Sq
 
 class Payment():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, senderAccountNo, receiverAccountNo, amount, date) -> None:
+        self.sender = senderAccountNo
+        self.reciever = receiverAccountNo
+        self.amount = amount
+        self.date = date
+        self.connPay = Sq.connect('payment.db').cursor()
+        #self.userObj = User()
+    
 
     def connect(self):
 
         """Documentation"""
-        pass
+        self.sender = self.userObj.validate(self.sender)
+        self.reciever = self.userObj.validate(self.sender)
+        return {'sender' : self.sender, 'receiver' : self.reciever}
+        
+
 
     def transaction(self):
 
         """Documentation"""
+        connObj = self.connect()
+        if(connObj['sender']['status'] == True and connObj['receiver']['status'] == True):
+            if(connObj['sender']['balance'] >= self.amount):
+                self.connPay.execute("INSERT INTO payments VALUES (?, ?, ?, ?)", (connObj['sender']['id'], connObj['receiver']['id'], self.amount, self.date))
+            else:
+                return {'status' : False, 'info' : "Not sufficient balance to perform transaction"}
+
+        else:
+            return {'status' : False, 'info' : "Unable to validate reciever"}
+
+
+
+        self.disconnect()
         pass
 
     def disconnect(self):
