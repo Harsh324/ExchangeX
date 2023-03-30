71520e93d06ae6b8ed9c0bde6454282a5cc9c27a
Harsh_324 <harshravi324@gmail.com>
2023-03-31 01:32:27 +0530

    kyc package done

---
+++ Kyc/kyc/kyc_functions.py
@@ -0,0 +1,42 @@
+import os
+import sqlite3 as Sq
+
+class Kyc():
+
+    def __init__(self, name, email, address, dob, docName, docNumber) -> None:
+        self.name = name
+        self.email = email
+        self.address = address
+        self.dob = dob
+        self.docName = docName
+        self.docNumber = docNumber
+
+        if not os.path.exists('database'):
+            os.makedirs('database')
+
+        self.kycObj = Sq.connect('kyc.db')
+
+
+    def addToDatabase(self):
+
+        self.kycObj.cursor().execute('''
+            CREATE TABLE IF NOT EXISTS kyc (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                email TEXT,
+                address TEXT,
+                dob TEXT,
+                doc_name TEXT,
+                doc_number TEXT
+            )
+        ''')
+
+        self.kycObj.cursor().execute('INSERT INTO kyc (name, email, address, dob, doc_name, doc_number) VALUES (?, ?, ?, ?, ?, ?)', (self.name, self.email, self.address, self.dob, self.doc_name, self.doc_number))
+        self.kycObj.commit()
+
+
+    def updateKyc(self):
+        pass
+
+
+    
